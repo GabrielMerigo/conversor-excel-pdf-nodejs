64cc31562c50536ef75ebc67da828b931ea4a102
Gabriel <gabriel.merigo.dev@gmail.com>
2022-02-09 01:01:53 -0300

    more accs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const Reader = require('./Reader');
 const Processor = require('./Processor');
+const Table = require('./Table');
 
 const leitor = new Reader();
 leitor.Read('./clients.csv').then(res => {
-  console.log(Processor.Process(res))
+  const users = new Table(Processor.Process(res))
+  console.log(users.header)
+
 }).catch(err => {
   console.log(err)
 })
