f0e41809e5fbd977cd408d5704d26b60b268a16b
Gabriel <gabriel.merigo.dev@gmail.com>
2022-02-09 22:52:42 -0300

    escritor de csv para html concluído

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const Reader = require('./Reader');
 const Processor = require('./Processor');
 const Table = require('./Table');
-const HtmlParser = require('./HtmlParser')
+const HtmlParser = require('./HtmlParser');
+const Writer = require('./Writter');
 
 const leitor = new Reader();
+const escritor = new Writer();
 
 async function main() {
   const data = await leitor.Read('./clients.csv')
   const users = new Table(Processor.Process(data));
   const html = await HtmlParser.Parser(users);
-  console.log(html)
+  escritor.Write(`${Date.now()}.html`, html)
 
 }
 
